a16919e28858db53e043fc9bd4fe3eb15dfc132e
xlp0rk0lx <hacker_dad@live.it>
2018-01-06 21:26:22 -0500

    first commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define FrameCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "FrameCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
